765f8241baf0499075bf8d952b91436581f89880
JoaoAndrioli <39911946+JoaoAndrioli@users.noreply.github.com>
2018-07-10 11:02:26 -0300

    Add files via upload

---
+++ Cinetica/paginas/menujs1.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $('span').click(function() {
+              $('.overlay').toggleClass('anim');
+          });
+
+          $('.animation').click(function(){
+              $('.anim').toggleClass('reverse-animation');
+          })
+});
